406c23728342e6008e227f60e4752a8aa1c688ca
nsdonosol <nsdonosol@gmail.com>
2025-07-02 23:18:51 +0200

    Error de compilación corregido

--- src/main/java/com/lauracercas/moviecards/MovieCardsApplication.java
+++ src/main/java/com/lauracercas/moviecards/MovieCardsApplication.java
@@ -1,7 +1,7 @@
 package com.lauracercas.moviecards;
 
 import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * Autor: Laura Cercas Ramos
